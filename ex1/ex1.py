d5af91b861481ca5ee765d258b9c9c8808a1f48c
Jordan Kennedy <jokennedy@g.harvard.edu>
2018-01-20 16:12:17 -0500

    Committing exercises 1 and 2 from Learn Python the Hard Way.

---
+++ ex1/ex1.py
@@ -0,0 +1,14 @@
+# Exercise 1: A Good First Program
+# The # character is a comment in t python, with it
+# you can get the interpreter to ignore lines.
+# This is the answer to Study Drill 3 and by inserting
+# a # in front of all of the print lines except one
+# you can complete Study Drill 2
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing'
+print "I'd much rather you 'not'"
+print 'I "said" do not touch this.'
+print "Let's add another." #Study Drill 1
---
+++ ex2/ex2.py
@@ -0,0 +1,14 @@
+# Exercise 2: Comments and Pound Characters
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+print "I could have code like this." # and the comment will be ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# print "This won't run."
+
+print "This will run."
+
+# Fun fact if # is inside quotes, like this: "#", it is part of a string
+# and can be output with a print command
+# try uncommenting the following line
+# print "See we can print # inside a string"
