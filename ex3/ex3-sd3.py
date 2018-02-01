ce9d664385db2fdcd5c1e432ffe3d0513e174b7c
Jordan Kennedy <jokennedy@g.harvard.edu>
2018-01-31 20:19:29 -0500

    Fixed Comment in ex3.py and added ex3-sd3.py for study drill 3

---
+++ ex3/ex3-sd3.py
@@ -0,0 +1,4 @@
+# Ex3-sd3: The file handling study drill 3 of exercise 3
+# Calculate how many hours a year I spend in my clarity
+print "Amount of time I spend driving to and from work:"
+print (.5+(42.0/60))*5*52-(.5+(42.0/60))*15, "hours"
